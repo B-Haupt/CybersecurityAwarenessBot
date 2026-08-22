9a1589947f9ddce9bf17d87703840632128ea2ee
B-Haupt <ST10500773@rcconnect.edu.za>
2026-08-22 16:36:35 +0200

    Add project files.

---
+++ CybersecurityAwarenessBot/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CybersecurityAwarenessBot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, there!");
+        }
+    }
+}
