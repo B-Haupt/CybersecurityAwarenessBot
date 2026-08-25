45ac46aa92919cb2a90887c3a25ca8e082707097
B-Haupt <ST10500773@rcconnect.edu.za>
2026-08-25 19:37:59 +0200

    Added the InputValidator class

---
+++ CybersecurityAwarenessBot/Bot/InputValidator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CybersecurityAwarenessBot.Bot
+{
+    internal class InputValidator
+    {
+        // returns false for null, empty or whitespaces
+        public bool isValidInput(string input) { 
+            return !string.IsNullOrWhiteSpace(input);
+        }
+
+        // Trims the extra space and then puts it to lowercase
+        public string NormaliseInput(string input) { 
+            return input.Trim().ToLower();
+        }
+
+    }
+}
